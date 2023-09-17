40e4fcf64a7a0ced9aa1c4c4222e93c367edb132
Meissa <meissasy@gmail.com>
2023-09-17 05:23:22 -0400

    Create Function To Analyze Round Winner

--- game.js
+++ game.js
@@ -3,6 +3,46 @@ let playerSelect = prompt("choose rock paper or scissors").toUpperCase();
 
 
 
+if (playerSelect ===  "ROCK" || playerSelect ===  "PAPER" || playerSelect ===  "SCISSORS"){
+    
+    let stage = playerSelect + getComputerChoice;
+
+    switch{
+            case stage === "ROCKSCISSORS": console.log("you win") 
+            break;
+            case stage === "SCISSORSROCK": console.log("you lose") 
+            break;
+            case stage === "ROCKPAPER": console.log("you lose") 
+            break;
+            case stage === "PAPERROCK": console.log("you win") 
+            break;
+            case stage === "SCISSORSPAPER": console.log("you win") 
+            break;
+            case stage === "PAPERSCISSORS": console.log("you lose") 
+            break;
+
+            default: console.log("tie")
+            break;
+
+
+
+
+
+        }
+
+
+}
+else{
+    console.log("pick a real option dweeb")
+}
+
+
+
+
+
+
+
+
 
 
 function getComputerChoice() {
